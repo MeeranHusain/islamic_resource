87836a1950758db3bfd6a09e84deb4378eb16516
Meeran Husain <90550959+MeeranHusain@users.noreply.github.com>
2021-10-21 16:11:43 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Rahman',
+        path: 'Surah_Rahman.m4a',
+        artist: 'Mishary Rashid Alafasy',
+        cover: 'image4.jpg'
+    },
+    {
+        name: 'Mulk',
+        path: 'Surah_Mulk.m4a',
+        artist: 'Mishary Rashid Alafasy',
+        cover: 'image1.jpg'
+    },
+    {
+        name: 'Muzzammil',
+        path: 'Surah_Muzzammil.mp3',
+        artist: 'Mishary Rashid Alafasy',
+        cover: 'image5.jpg'
+    },
+    {
+        name: 'Al-Fajr',
+        path: 'Surah_Al-Fajr.mp3',
+        artist: 'Mishary Rashid Alafasy',
+        cover: 'image8.jpg'
+    },
+    {
+        name: 'Aytul_Kursi',
+        path: 'Aytul_Kursi.mp3',
+        artist: 'Mishary Rashid Alafasy',
+        cover: 'image6.jpg'
+    },
+    {
+        name: '4 Qul',
+        path: '4_Surah_Qul_With_Complete_Tarjuma__Al-Kafiroun__Al-Ikhlas__Al-Falaq__An-Nas__ARY_Qtv(256k).mp3',
+        artist: 'ARY Qtv',
+        cover: 'image7.jpg'
+    },
+    {
+        name: 'Nabi Name',
+        path: 'Muhammed_S.A.W_Nabi_Ul_Husna.m4a',
+        artist: 'ARY Qtv',
+        cover: 'image2.jpg'
+    },
+    {
+        name: 'Allah Name',
+        path: 'Asma-ul-Husna.m4a',
+        artist: 'Atif Aslam',
+        cover: 'image3.jpeg'
+    },
+]
